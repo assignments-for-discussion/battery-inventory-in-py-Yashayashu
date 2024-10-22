665d1374017a4b86427b6404d7a10bd63e4ea0bf
Yashaswini B V <125262149+Yashayashu@users.noreply.github.com>
2024-10-22 17:06:15 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,23 @@
 
 def count_batteries_by_health(present_capacities):
-  return {
+  rated_capacity=120
+  health_counts= {
     "healthy": 0,
     "exchange": 0,
     "failed": 0
-  }
+  } 
+  for present_capacity in present_capacities:
+      
+        soh = 100 * present_capacity / rated_capacity
+       
+        if soh > 80:
+            health_counts["healthy"] += 1
+        elif 62 < soh <= 80:
+            health_counts["exchange"] += 1
+        else:
+            health_counts["failed"] += 1
+
+  return health_counts
 
 
 def test_bucketing_by_health():
